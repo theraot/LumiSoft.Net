3062ab115e039f15446887eeb584bab43d3d43d0
Alfonso J. Ramos <theraot@gmail.com>
2019-06-18 11:38:52 -0500

    STUN_Result documentation

--- Net/STUN/Client/STUN_Result.cs
+++ Net/STUN/Client/STUN_Result.cs
@@ -2,16 +2,30 @@
 
 namespace LumiSoft.Net.STUN.Client
 {
+    /// <summary>
+    ///     This class holds STUN_Client.Query method return data.
+    /// </summary>
     public class STUN_Result
     {
+        /// <summary>
+        ///     Default constructor.
+        /// </summary>
+        /// <param name="netType">Specifies UDP network type.</param>
+        /// <param name="publicEndPoint">Public IP end point.</param>
         public STUN_Result(STUN_NetType netType, IPEndPoint publicEndPoint)
         {
             NetType = netType;
             PublicEndPoint = publicEndPoint;
         }
 
+        /// <summary>
+        ///     Gets UDP network type.
+        /// </summary>
         public STUN_NetType NetType { get; }
 
+        /// <summary>
+        ///     Gets public IP end point. This value is null if failed to get network type.
+        /// </summary>
         public IPEndPoint PublicEndPoint { get; }
     }
 }
