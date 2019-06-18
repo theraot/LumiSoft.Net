8cc1cbbb478b77ca7ae8a8242dfd5e73db37cc32
Alfonso J. Ramos <theraot@gmail.com>
2019-06-18 12:30:06 -0500

    STUN_t_ChangeRequest documentation

--- Net/STUN/Message/STUN_t_ChangeRequest.cs
+++ Net/STUN/Message/STUN_t_ChangeRequest.cs
@@ -1,16 +1,30 @@
 ﻿namespace LumiSoft.Net.STUN.Message
 {
+    /// <summary>
+    ///     This class implements STUN CHANGE-REQUEST attribute. Defined in RFC 3489 11.2.4.
+    /// </summary>
     // ReSharper disable once InconsistentNaming
     internal class STUN_t_ChangeRequest
     {
+        /// <summary>
+        ///     Default constructor.
+        /// </summary>
+        /// <param name="changeIP">Specifies if STUN server must send response to different IP than request was received.</param>
+        /// <param name="changePort">Specifies if STUN server must send response to different port than request was received.</param>
         public STUN_t_ChangeRequest(bool changeIP, bool changePort)
         {
             ChangeIP = changeIP;
             ChangePort = changePort;
         }
 
+        /// <summary>
+        ///     Gets or sets if STUN server must send response to different IP than request was received.
+        /// </summary>
         public bool ChangeIP { get; }
 
+        /// <summary>
+        ///     Gets or sets if STUN server must send response to different port than request was received.
+        /// </summary>
         public bool ChangePort { get; }
     }
 }
