973e9a7ecf043c04bf9c676b9cd56004ce743703
Alfonso J. Ramos <theraot@gmail.com>
2019-06-18 09:48:15 -0500

    STUN_Client public members

--- Net/STUN/Client/STUN_Client.cs
+++ Net/STUN/Client/STUN_Client.cs
@@ -1,10 +1,85 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Net;
+using System.Net.Sockets;
 
 namespace LumiSoft.Net.STUN.Client
 {
-    class STUN_Client
+    /// <summary>
+    /// This class implements STUN client. Defined in RFC 3489.
+    /// </summary>
+    /// <example>
+    /// <code>
+    /// // Create new socket for STUN client.
+    /// Socket socket = new Socket(AddressFamily.InterNetwork,SocketType.Dgram,ProtocolType.Udp);
+    /// socket.Bind(new IPEndPoint(IPAddress.Any,0));
+    /// // Query STUN server
+    /// STUN_Result result = STUN_Client.Query("stunserver.org",3478,socket);
+    /// if(result.NetType != STUN_NetType.UdpBlocked){
+    ///     // UDP blocked or !!!! bad STUN server
+    /// }
+    /// else{
+    ///     IPEndPoint publicEP = result.PublicEndPoint;
+    ///     // Do your stuff
+    /// }
+    /// </code>
+    /// </example>
+    public class STUN_Client
     {
+        /// <summary>
+        /// Resolves socket local end point to public end point.
+        /// </summary>
+        /// <param name="stunServer">STUN server.</param>
+        /// <param name="port">STUN server port. Default port is 3478.</param>
+        /// <param name="socket">UDP socket to use.</param>
+        /// <returns>Returns public IP end point.</returns>
+        /// <exception cref="T:System.ArgumentNullException">Is raised when <b>stunServer</b> or <b>socket</b> is null reference.</exception>
+        /// <exception cref="T:System.ArgumentException">Is raised when any of the arguments has invalid value.</exception>
+        /// <exception cref="T:System.IO.IOException">Is raised when no connection to STUN server.</exception>
+        public static IPEndPoint GetPublicEP(string stunServer, int port, Socket socket)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Resolves local IP to public IP using STUN.
+        /// </summary>
+        /// <param name="stunServer">STUN server.</param>
+        /// <param name="port">STUN server port. Default port is 3478.</param>
+        /// <param name="localIP">Local IP address.</param>
+        /// <returns>Returns public IP address.</returns>
+        /// <exception cref="T:System.ArgumentNullException">Is raised when <b>stunServer</b> or <b>localIP</b> is null reference.</exception>
+        /// <exception cref="T:System.ArgumentException">Is raised when any of the arguments has invalid value.</exception>
+        /// <exception cref="T:System.IO.IOException">Is raised when no connection to STUN server.</exception>
+        public static IPAddress GetPublicIP(string stunServer, int port, IPAddress localIP)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets NAT info from STUN server.
+        /// </summary>
+        /// <param name="host">STUN server name or IP.</param>
+        /// <param name="port">STUN server port. Default port is 3478.</param>
+        /// <param name="localEP">Local IP end point.</param>
+        /// <returns>Returns UDP netwrok info.</returns>
+        /// <exception cref="T:System.ArgumentNullException">Is raised when <b>host</b> or <b>localEP</b> is null reference.</exception>
+        /// <exception cref="T:System.Exception">Throws exception if unexpected error happens.</exception>
+        public static STUN_Result Query(string host, int port, IPEndPoint localEP)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets NAT info from STUN server.
+        /// </summary>
+        /// <param name="host">STUN server name or IP.</param>
+        /// <param name="port">STUN server port. Default port is 3478.</param>
+        /// <param name="socket">UDP socket to use.</param>
+        /// <returns>Returns UDP netwrok info.</returns>
+        /// <exception cref="T:System.Exception">Throws exception if unexpected error happens.</exception>
+        public static STUN_Result Query(string host, int port, Socket socket)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Net/STUN/Client/STUN_Result.cs
+++ Net/STUN/Client/STUN_Result.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace LumiSoft.Net.STUN.Client
 {
-    class STUN_Result
+    public class STUN_Result
     {
     }
 }
