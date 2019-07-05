3ce9db91ea26153b01e38ec055ccb790eb585e3b
Alfonso J. Ramos <theraot@gmail.com>
2019-07-04 22:37:48 -0500

    Added TraceRoute

---
+++ Net/TraceNode.cs
@@ -0,0 +1,24 @@
+﻿#pragma warning disable ET002 // Namespace does not match file path or default namespace
+
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace Theraot.Net
+{
+    [System.Diagnostics.DebuggerNonUserCode]
+    public class TraceNode
+    {
+        internal TraceNode(IPAddress address, IPStatus status, int? ttl)
+        {
+            Address = address;
+            Status = status;
+            Ttl = ttl;
+        }
+
+        public int? Ttl { get; }
+
+        public IPAddress Address { get; }
+
+        public IPStatus Status { get; }
+    }
+}
---
+++ Net/TraceRoute.cs
@@ -0,0 +1,58 @@
+﻿#pragma warning disable ET002 // Namespace does not match file path or default namespace
+
+using System;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace Theraot.Net
+{
+    [System.Diagnostics.DebuggerNonUserCode]
+    public static class TraceRoute
+    {
+        public static void Trace(IPAddress destination, Func<IPAddress, TraceNode, bool> next, Action completed)
+        {
+            const int bufferSize = 32;
+            const int timeoutMilliseconds = 1000;
+
+            if (destination == null)
+            {
+                throw new ArgumentNullException(nameof(destination));
+            }
+            if (next == null)
+            {
+                throw new ArgumentNullException(nameof(next));
+            }
+            if (completed == null)
+            {
+                completed = () => { };
+            }
+            var buffer = new byte[bufferSize];
+            var options = new PingOptions(1, true);
+            var pings = new []{new Ping()};
+            pings[0].PingCompleted += OnPingCompleted;
+            pings[0].SendAsync(destination, timeoutMilliseconds, buffer, options, null);
+
+            void OnPingCompleted(object sender, PingCompletedEventArgs e)
+            {
+                var address = e.Reply.Address;
+                var status = e.Reply.Status;
+                var done = status == IPStatus.TimedOut
+                           || !next.Invoke(destination, new TraceNode(address, status, e.Reply.Options?.Ttl))
+                           || status == IPStatus.Success
+                           || address.Equals(destination);
+                if (done)
+                {
+                    pings[0].Dispose();
+                    pings[0] = null;
+                    completed();
+                    completed = null;
+                }
+                else
+                {
+                    options.Ttl++;
+                    pings[0].SendAsync(destination, timeoutMilliseconds, buffer, options, null);
+                }
+            }
+        }
+    }
+}
